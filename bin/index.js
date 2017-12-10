0f653cd128f444ead31d954040368b5c2c190c56
Ben Baumann <ben@thisisbam.com>
2017-12-10 14:31:24 -0500

    fixed twitter suspension

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,4 @@
+const say = require('say');
 const liri = require('../lib/liri');
 const format = require('../lib/format');
 const log = require('../lib/log');
@@ -7,6 +8,7 @@ liri[process.argv[2].toLowerCase()]((err, res)=>{
         console.log(format(res));
         log(format(res), err => {
             if(err) console.log(err);
+            say.speak('I hope you like your results, please check the log.txt file');
         });
     }, process.argv[3]
 );
